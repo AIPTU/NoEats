0e340a5866564beaa6ada59f0c26d05628a412fb
AIPTU <anangp808@gmail.com>
2022-01-06 23:55:43 +0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # NoEats
-A PocketMine-MP plugin to regulate player hunger and prohibit eating certain foods easily
+
+[![Discord](https://img.shields.io/discord/830063409000087612?color=7389D8&label=discord)](https://discord.com/invite/EggNF9hvGv)
+
+A PocketMine-MP plugin to regulate player hunger and prohibit eating certain foods easily.
+
+# Features
+
+- Managing hunger.
+- Permission bypass.
+- Supports `&` as formatting codes.
+- Custom item that can't be eat.
+- Per world support.
+- Lightweight and open source ❤️
+
+# Permissions
+
+- Permission `noeats.bypass` allows the user to bypass eating.
+- Permission `hunger.bypass` allows the user to bypass hunger.
+
+# Default Config
+```yaml
+---
+# Do not change this (Only for internal use)!
+config-version: 1.0
+
+# If you want to activate player hunger.
+hunger: true
+
+# Message used when canceling a player who ate food.
+# Use "§" or "&" to color the message.
+message: "&cYou can't eat your food here"
+
+items:
+  # List of items that can't be eat.
+  # This must be food to work.
+  # If the item has meta, you can use the format "minecraft:id:meta".
+  list:
+    - "minecraft:golden_apple"
+    - "minecraft:cooked_beef"
+    - "minecraft:cooked_chicken"
+
+worlds:
+  # The mode can be either "blacklist" or "whitelist".
+  # The blacklist mode will only cancel players to eat and players to be hungry according to the name of a predetermined world folder and will allow player feeding and hunger players around the world.
+  # The whitelist mode will only allow players to eat and players to be hungry according to the name of a predetermined world folder and will cancel player feeding and starve players around the world.
+  mode: "blacklist"
+  # List of world folder names to blacklist/whitelist (depending on the mode set above).
+  # Leave it blank if you want to let players eat all over the world.
+  list:
+    - "world"
+...
+
+```
+
+# Upcoming Features
+
+- Currently none planned. You can contribute or suggest for new features.
+
+# Additional Notes
+
+- If you find bugs or want to give suggestions, please visit [here](https://github.com/AIPTU/NoEats/issues).
+- We accept any contributions! If you want to contribute please make a pull request in [here](https://github.com/AIPTU/NoEats/pulls).
+- Icons made from [www.flaticon.com](https://www.flaticon.com)
---
+++ src/NoEats.php
@@ -0,0 +1,173 @@
+<?php
+
+/*
+ *
+ * Copyright (c) 2021 AIPTU
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace aiptu\noeats;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\player\PlayerItemConsumeEvent;
+use pocketmine\item\Food;
+use pocketmine\item\Item;
+use pocketmine\item\LegacyStringToItemParser;
+use pocketmine\item\LegacyStringToItemParserException;
+use pocketmine\item\StringToItemParser;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\world\World;
+use function explode;
+use function in_array;
+use function rename;
+use function str_replace;
+use function trim;
+
+final class NoEats extends PluginBase implements Listener
+{
+	private const CONFIG_VERSION = 1.0;
+
+	private const MODE_BLACKLIST = 0;
+	private const MODE_WHITELIST = 1;
+
+	private int $mode;
+
+	private ConfigProperty $configProperty;
+
+	public function onEnable(): void
+	{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+		$this->checkConfig();
+	}
+
+	public function onPlayerExhaust(PlayerExhaustEvent $event): void
+	{
+		if ($event->isCancelled()) {
+			return;
+		}
+
+		$player = $event->getPlayer();
+		if (!$player instanceof Player) {
+			return;
+		}
+
+		if (!$this->getConfigProperty()->getPropertyBool('hunger', true)) {
+			if (!$this->checkWorld($player->getWorld())) {
+				return;
+			}
+
+			if ($player->hasPermission('hunger.bypass')) {
+				return;
+			}
+
+			$event->cancel();
+		}
+	}
+
+	public function onPlayerItemConsume(PlayerItemConsumeEvent $event): void
+	{
+		if ($event->isCancelled()) {
+			return;
+		}
+
+		$player = $event->getPlayer();
+		$item = $event->getItem();
+
+		foreach ($this->getConfigProperty()->getPropertyArray('items.list', []) as $value) {
+			$itemEats = $this->checkItem($value);
+			if (!$itemEats instanceof Food) {
+				continue;
+			}
+
+			if ($item->equals($itemEats)) {
+				if (!$this->checkWorld($player->getWorld())) {
+					return;
+				}
+
+				if ($player->hasPermission('noeats.bypass')) {
+					return;
+				}
+
+				$player->sendPopup(TextFormat::colorize($this->getConfigProperty()->getPropertyString('message', "&cYou can't eat your food here")));
+				$event->cancel();
+			}
+		}
+	}
+
+	public function getConfigProperty(): ConfigProperty
+	{
+		return $this->configProperty;
+	}
+
+	private function checkConfig(): void
+	{
+		$this->saveDefaultConfig();
+
+		if (!$this->getConfig()->exists('config-version') || ($this->getConfig()->get('config-version', self::CONFIG_VERSION) !== self::CONFIG_VERSION)) {
+			$this->getLogger()->notice('Your configuration file is outdated, updating the config.yml...');
+			$this->getLogger()->notice('The old configuration file can be found at config.old.yml');
+
+			rename($this->getDataFolder() . 'config.yml', $this->getDataFolder() . 'config.old.yml');
+
+			$this->reloadConfig();
+		}
+
+		$this->configProperty = new ConfigProperty($this->getConfig());
+
+		foreach ($this->getConfigProperty()->getPropertyArray('items.list', []) as $value) {
+			$this->checkItem($value);
+		}
+
+		match ($this->getConfigProperty()->getPropertyString('worlds.mode', 'blacklist')) {
+			'blacklist' => $this->mode = self::MODE_BLACKLIST,
+			'whitelist' => $this->mode = self::MODE_WHITELIST,
+			default => throw new \InvalidArgumentException('Invalid mode selected, must be either "blacklist" or "whitelist"!'),
+		};
+	}
+
+	private function checkItem(string $string): Item
+	{
+		try {
+			$item = LegacyStringToItemParser::getInstance()->parse($string);
+		} catch (LegacyStringToItemParserException $e) {
+			if (($item = StringToItemParser::getInstance()->parse(explode(':', str_replace([' ', 'minecraft:'], ['_', ''], trim($string)))[0])) === null) {
+				throw $e;
+			}
+		}
+
+		return $item;
+	}
+
+	private function checkWorld(World $world): bool
+	{
+		if ($this->mode === self::MODE_BLACKLIST) {
+			return !(in_array($world->getFolderName(), $this->getConfigProperty()->getPropertyArray('worlds.list', []), true));
+		}
+
+		return in_array($world->getFolderName(), $this->getConfigProperty()->getPropertyArray('worlds.list', []), true);
+	}
+}
